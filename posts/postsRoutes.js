4a32eaa223b1017b230310601c7b6aaabe9a55f9
Paul M Edwards <pauledwards@gmail.com>
2020-01-19 23:49:24 -0800

    Implement all post & get calls.

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -28,7 +28,20 @@ router.post('/', (req, res) => {
   */
   //#endregion Post Posts Summary
 
-  console.log(`POST ${apiBase}/ insert():\n`, req);
+  // console.log(`POST ${apiBase}/ insert():\n`, req);
+
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({ success: false, errorMessage: "Please provide title and contents for the post." });
+  } else {
+    db.insert(req.body)
+      .then(post => {
+        console.log(`POST ${apiBase}/ insert(): \n`, post);
+        res.status(201).json({ success: true, post: post });
+      })
+      .catch(err => {
+        res.status(500).json({ success: false, errorMessage: "There was an error while saving the post to the database." });
+      });
+  }
 });
 
 router.post('/:postId/comments', (req, res) => {
@@ -57,7 +70,34 @@ router.post('/:postId/comments', (req, res) => {
   */
   //#endregion Post Comments Summary
 
-  console.log(`POST ${apiBase}/:postId/comments insertComment():\n`, req);
+  const { postId } = req.params;
+
+  // console.log(`POST ${apiBase}/:postId/comments insertComment(${postId}):\n`, req);
+
+  if (!req.body.text) {
+    res.status(400).json({ success: false, errorMessage: "Please provide text for the comment." });
+  } else {
+    db.findById(postId)
+      .then(post => {
+        if (post) {
+          db.findPostComments(postId)
+            .then(comments => {
+              req.body.post_id = postId;
+              // console.log(`POST ${apiBase}/:postId/comments insertComment(${postId}): \n`, req.body);
+              db.insertComment(req.body)
+                .then(comment => {
+                  console.log(`POST ${apiBase}/:postId/comments insertComment(${postId}): \n`, comment);
+                  res.status(201).json({ success: true, comment: comment });
+                })
+                .catch(err => {
+                  res.status(500).json({ success: false, errorMessage: "There was an error while saving the comment to the database." });
+                });
+            });
+        } else {
+          res.status(404).json({ success: false, errorMessage: "The post with the specified ID does not exist." });
+        }
+      });
+  }
 });
 
 router.get('/', (req, res) => {
@@ -72,7 +112,16 @@ router.get('/', (req, res) => {
   */
   //#endregion Get Posts Summary
 
-  console.log(`GET ${apiBase}/ find():\n`, req);
+  // console.log(`GET ${apiBase}/ find():\n`, req);
+
+  db.find()
+    .then(posts => {
+      console.log(`GET ${apiBase}/ find():\n`, posts);
+      res.status(200).json({ success: true, posts: posts });
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, errorMessage: "The posts information could not be retrieved."});
+    });
 });
 
 router.get('/:postId', (req, res) => {
@@ -91,7 +140,22 @@ router.get('/:postId', (req, res) => {
   */
   //#endregion Get Post by ID Summary
 
-  console.log(`GET ${apiBase}/:postId findById():\n`, req);
+  const { postId } = req.params;
+
+  // console.log(`GET ${apiBase}/:postId findById(${postId}):\n`, req);
+
+  db.findById(postId)
+    .then(post => {
+      console.log(`GET ${apiBase}/:postId findById(${postId}): \n`, post);
+      if (post) {
+        res.status(200).json({ success: true, post });
+      } else {
+        res.status(404).json({ success: false, errorMessage: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, errorMessage: "The post information could not be retrieved." });
+    });
 });
 
 router.get('/:postId/comments', (req, res) => {
@@ -110,17 +174,63 @@ router.get('/:postId/comments', (req, res) => {
   */
   //#endregion Get Comments by Post ID Summary
 
-  console.log(`GET ${apiBase}/:postId/comments findPostComments():\n`, req);
+  const { postId } = req.params;
+
+  // console.log(`GET ${apiBase}/:postId/comments findPostComments(${postId}):\n`, req);
+
+  db.findById(postId)
+    .then(post => {
+      if (post) {
+        db.findPostComments(postId)
+        .then(comments => {
+          console.log(`GET ${apiBase}/:postId/comments findPostComments(${postId}): \n`, comments);
+          if (comments) {
+            res.status(200).json({ success: true, comments: comments });
+          } else {
+            res.status(404).json({ success: false, errorMessage: "The post has no comments." });
+          }
+        })
+        .catch(err => {
+          res.status(500).json({ success: false, errorMessage: "The comments information could not be retrieved." });
+        });
+      } else {
+        res.status(404).json({ success: false, errorMessage: "The post with the specified ID does not exist." });
+      }
+    })
 });
 
-router.get('/:postId/comments/:commentId', (req, res) => {
-  //#region Get Comment by ID within Post by ID Summary
+router.get('/comments/:commentId', (req, res) => {
+  //#region Get Comment by ID Summary
   /*
-    TBD
-  */
-  //#endregion Get Comment by ID within Post by ID Summary
+    When the client makes a `GET` request to `/comments/:id`:
 
-  console.log(`GET ${apiBase}/:postId/comments/:commentId findCommentById():\n`, req);
+    - If the _comment_ with the specified `id` is not found:
+      - return HTTP status code `404` (Not Found).
+      - return the following JSON object: `{ message: "The comment with the specified ID does not exist." }`.
+
+    - If there's an error in retrieving the _comment_ from the database:
+      - cancel the request.
+      - respond with HTTP status code `500`.
+      - return the following JSON object: `{ error: "The comment information could not be retrieved." }`.
+  */
+  //#endregion Get Comment by ID Summary
+
+  const { commentId } = req.params;
+
+  // console.log(`GET ${apiBase}/comments/:commentId findCommentById(${commentId}):\n`, req);
+
+  db.findCommentById(commentId)
+    .then(comment => {
+      console.log(`GET ${apiBase}/comments/:commentId findCommentById(${commentId}): \n`, comment);
+      if (comment) {
+        res.status(200).json({ success: true, comment: comment });
+      } else {
+        res.status(404).json({ success: false, errorMessage: "The comment with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, errorMessage: "The comment information could not be retrieved." });
+    });
 });
 
 router.put('/:postId', (req, res) => {
